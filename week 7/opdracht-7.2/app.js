836439934f00a81477b493e63d3300b46ab159f1
FabioSalis <9019469@student.zadkine.nl>
2022-10-07 09:50:55 +0200

    opdracht-7.2

---
+++ week 7/opdracht-7.2/app.js
@@ -0,0 +1,11 @@
+let budget = prompt("hoeveel geld heeft u")
+let product = 60
+function koop() {
+if (product <= budget) {
+    document.getElementById("text").innerText = "U heeft genoeg geld!"
+    document.getElementById("text").style.color = "green"
+} else {
+    document.getElementById("text").innerText = "Helaas, u heeft niet genoeg geld"
+    document.getElementById("text").style.color = "red"
+}
+}
